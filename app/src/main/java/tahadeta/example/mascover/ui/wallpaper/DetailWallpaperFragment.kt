8167704d5940d12ed89e3efc46620c15d350d59c
Taha Jadid <taha.jadid@inwi.ma>
2022-07-20 15:23:57 +0100

    Fix Toast message

--- app/src/main/java/tahadeta/example/mascover/ui/wallpaper/DetailWallpaperFragment.kt
+++ app/src/main/java/tahadeta/example/mascover/ui/wallpaper/DetailWallpaperFragment.kt
@@ -131,7 +131,6 @@ class DetailWallpaperFragment : Fragment() {
             demoOneCl.visibility = View.GONE
             showDemoTwo()
         }
-
     }
 
     private fun showDemoTwo() {
@@ -235,7 +234,7 @@ class DetailWallpaperFragment : Fragment() {
                 Toast.makeText(activity, Constants.END_DOWNLOAD_FR, Toast.LENGTH_SHORT).show()
             }
         } else {
-            Toast.makeText(activity, "Permission Rejetée, Activez la permission", Toast.LENGTH_LONG).show()
+            Toast.makeText(activity, "Activez la permission", Toast.LENGTH_SHORT).show()
         }
     }
 
@@ -272,7 +271,7 @@ class DetailWallpaperFragment : Fragment() {
             }
         } else {
             progressBar.visibility = View.GONE
-            Toast.makeText(activity, "Permission Rejetée, Activez la permission", Toast.LENGTH_LONG).show()
+            Toast.makeText(activity, "Permission Rejetée, Activez la permission", Toast.LENGTH_SHORT).show()
         }
     }
 
@@ -314,7 +313,7 @@ class DetailWallpaperFragment : Fragment() {
                         Toast.makeText(activity, Constants.END_DOWNLOAD_FR, Toast.LENGTH_SHORT).show()
                     }
                 } else {
-                    Toast.makeText(activity, "Permission Rejetée, Activez la permission", Toast.LENGTH_LONG).show()
+                    Toast.makeText(activity, "Permission Rejetée, Activez la permission", Toast.LENGTH_SHORT).show()
                 }
             }
         } else {
@@ -391,16 +390,6 @@ class DetailWallpaperFragment : Fragment() {
 
         // Animate the alpha value to 1F and set duration
         view.animate().alpha(1F).translationYBy(-spaceWithPixel).setDuration(duration)
-    }
-
-    fun fadeToDown(view: View, spaceWithPixel: Float, duration: Long) {
-
-        // Set button alpha to the 0
-        view.alpha = 0F
-        view.translationY = -spaceWithPixel
-
-        // Animate the alpha value to 1F and set duration
-        view.animate().alpha(1F).translationYBy(spaceWithPixel).setDuration(duration)
     }
 
 }
